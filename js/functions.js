63e1281013ca073ceb8718a2323025fb2eea385c
gtorres <gerardo.torres@sifei.com.mx>
2022-02-25 21:08:19 -0600

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,38 @@
+var options = {
+  strings: ["Barato.", "Ilimitado."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 100,
+		// time before typing starts
+		startDelay: 500,
+		// backspacing speed
+		backSpeed: 100,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+    loopCount: Infinity,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+};
+
+const typed = new Typed('.typed', options);
+
+
